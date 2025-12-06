3703c24c9eac569b1d1d1b7a2fdd2b310830abb3
Санталов Артур <arthur@MacBook-Pro-Santalov.local>
2025-12-06 20:29:48 +0400

    feat: добавил entity для track

--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TrackService } from './track.service';
 import { TrackController } from './track.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Track } from './track.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([Track])],
   providers: [TrackService],
   controllers: [TrackController],
 })
