1b88d763e158298a8e9c38441e605d06d5b17d1a
Санталов Артур <arthur@MacBook-Pro-Santalov.local>
2025-12-09 00:44:45 +0400

    feat: добавил auth ручку

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
 import { FavoriteController } from './favorite/favorite.controller';
 import { FavoriteModule } from './favorite/favorite.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { FavoriteModule } from './favorite/favorite.module';
     TrackModule,
     AlbumModule,
     FavoriteModule,
+    AuthModule,
   ],
   controllers: [FavoriteController],
   providers: [],
